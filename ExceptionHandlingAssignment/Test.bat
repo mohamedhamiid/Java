fe29dcd1cd2490b0309c719e050e807a037e1218
Mohamed Abdel Hamid <111571591+mohamedhamiid@users.noreply.github.com>
2023-04-19 17:59:14 +0200

    Add files via upload

---
+++ ExceptionHandlingAssignment/Test.bat
@@ -0,0 +1,12 @@
+set path = "C:\Program Files\Java\jdk-19\bin";
+
+javac ContainerSorting.java
+java ContainerSorting.java Normal.arxml
+
+javac ContainerSorting.java
+java ContainerSorting.java Empty.arxml
+
+javac ContainerSorting.java
+java ContainerSorting.java NotValid.xml
+
+pause
